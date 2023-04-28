87e688687d58fcea9bb2ac9a090a5ab396824dfb
YoussefElbanna <yousseflebanna88@gmail.com>
2023-04-27 19:34:03 -0700

    update

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,5 +1,5 @@
 #include "lists.h"
-int _str(char *s)
+int _strlen(char *s)
 {	
 	int i = 0;
 	if (!s)
@@ -13,7 +13,7 @@ size_t print_list(const list_t *h)
 	size_t i = 0;
 	while (h)
 	{
-		printf("[%d] %s\n", _str(h->str), h-> str ? h ->str :"(nil)";
+		printf("[%d] %s\n", _strlen(h->str), h-> str ? h ->str :"(nil)";
 		h = h->next;
 		i++
 	}
